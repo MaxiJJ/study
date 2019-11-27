f0e58241688de85e601672241b9b66ef1ca306e3
maxij <maximgri93@gmail.com>
2019-11-27 19:55:29 +0300

    Add first class

---
+++ src/Example.java
@@ -0,0 +1,10 @@
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+//              ctrl + alt + L - форматирование кода
+    }
+}
